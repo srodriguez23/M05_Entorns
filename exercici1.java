8165cb3e5919f1f64273332544e2a3cf813114d8
srodriguez23 <144943302+srodriguez23@users.noreply.github.com>
2024-01-11 20:20:38 +0100

    Update PRIMER CAMBIO exercici1.java

--- exercici1.java
+++ exercici1.java
@@ -14,8 +14,7 @@ public class exercici1 {
      //Para generar el random 
      
          int min=0;
-         int max=100;
-     
+      
          for (int i=0; i< 10; i++) {
             
           array[i] = (int) Math.random() * ((max-min + 1)) + min;
